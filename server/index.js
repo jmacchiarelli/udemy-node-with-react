5fdca1fb8fab7257d4bb49d92ccfea846224413a
Jennifer Macchiarelli <jennifermacchiarelli@Jennifers-MacBook.local>
2020-02-06 15:01:56 -0500

    trying to sort out setup

--- server/index.js
+++ server/index.js
@@ -1,20 +1,12 @@
-// creating a brand new express application and route handler
-
-// import express library, common JS modules for requiring or sharing code between files
+// create a new express app
 const express = require('express');
-
-// calling express like a function generates a new application that represents a running express app
 const app = express();
 
-// route handler and associate with a given route
+// create a route handler, associate with given route
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-// dynamic port adding in prod, heroku will later inject env variables in runtime, pass runtime config
+// dynamic port binding
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-
-
-// specicy start script,
