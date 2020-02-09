b4a52d26c922b5bcdaa8205cbe33d13f6f58a211
Jennifer Macchiarelli <jenmacdev@gmail.com>
2020-02-09 11:33:02 -0500

    refactoring routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,15 @@
+// requiring original passport npm module
+const passport = require('passport');
+
+module.exports = () => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+    scope: ['profile', 'email']
+    })
+  );
+
+  // second route handler for exchange for user profile
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+};
