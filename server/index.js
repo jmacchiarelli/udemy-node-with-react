0770def4abbddad2586e5ad9c83f5e3ada4cf1f3
Jennifer Macchiarelli <jenmacdev@gmail.com>
2020-02-08 22:51:23 -0500

    changing greeting

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const app = express();
 
 // create a route handler, associate with given route
 app.get('/', (req, res) => {
-  res.send({ go: 'memphis grizzlies' });
+  res.send({ go: 'OKC Thunder' });
 });
 
 // dynamic port binding
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// testing heroku deployment with github repo connectivity
