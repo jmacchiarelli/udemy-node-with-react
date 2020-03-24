aacec2ccc2d9373ea8090152d24247670348b8fe
Jennifer Macchiarelli <jenmacdev@gmail.com>
2020-03-23 21:19:14 -0400

    clean up to carry on

--- server/index.js
+++ server/index.js
@@ -1,40 +1,9 @@
-// create a new express app
-// adding passport with google oauth strategy
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+require('./routes/authRoutes')(app);
 
-// function call to create a new instance of the strategy
-// use keys for google api in strategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
-// route handler for google ouath process in passport
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-  scope: ['profile', 'email']
-  })
-);
-
-// second route handler for exchange for user profile
-app.get('/auth/google/callback', passport.authenticate('google'));
-
-// dynamic port binding
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 // requiring original passport npm module
 const passport = require('passport');
 
-module.exports = () => {
+module.exports = app => {
   app.get(
     '/auth/google',
     passport.authenticate('google', {
