32eb8fdc23ba831f00fc0d5d1e57827d1d2dd86e
Jennifer Macchiarelli <jenmacdev@gmail.com>
2020-03-23 21:55:13 -0400

    finish routes refactor to move forward

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 require('./services/passport');
-const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,15 +1,15 @@
 // requiring original passport npm module
 const passport = require('passport');
 
+
 module.exports = app => {
   app.get(
     '/auth/google',
     passport.authenticate('google', {
-    scope: ['profile', 'email']
+      scope: ['profile', 'email']
     })
   );
 
   // second route handler for exchange for user profile
   app.get('/auth/google/callback', passport.authenticate('google'));
-
 };
